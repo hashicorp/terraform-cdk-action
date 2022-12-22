5e126f8de2409c398d8e7b25f82680cf0fd4e0fa
Nara Kasbergen <nara.kasbergen@gmail.com>
2022-12-22 13:18:59 +0100

    chore: implement Ansgar's solution for licenses through Projen
    This brings the license generation into the Projen template to
    ensure that it does not get overwritten in the future, copying
    Ansgar's implementation from the AWS Adapter. Note that this
    project is currently using an Apache-2.0 license where HashiCorp's
    standard has been MPL-2.0 and I don't know if using Apache here is
    intentional or an accidental oversight. But, we can always fix
    that in a follow-up PR.

---
+++ projenrc/customized-license.ts
@@ -0,0 +1,19 @@
+import { IResolver, License } from "projen";
+import { TypeScriptProject } from "projen/lib/typescript";
+
+const SPDX = "Apache-2.0";
+
+export class CustomizedLicense extends License {
+  constructor(project: TypeScriptProject) {
+    super(project, { spdx: SPDX });
+
+    project.addFields({ license: SPDX });
+  }
+
+  synthesizeContent(resolver: IResolver) {
+    return (
+      "Copyright (c) 2022 HashiCorp, Inc.\n\n" +
+      super.synthesizeContent(resolver)
+    );
+  }
+}
