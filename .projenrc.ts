820d6ef42e93847ba5ec096fccc84cff27ce1df3
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-05-30 15:59:12 +0200

    chore: pin action-semantic-pull-request

--- .projenrc.ts
+++ .projenrc.ts
@@ -16,7 +16,8 @@ const githubActionPinnedVersions = {
   "actions/upload-artifact": "0b7f8abb1508181956e8e162db84b466c27e18ce", // v3.1.2
   "actions/download-artifact": "9bc31d5ccc31df68ecc42ccf4149144866c47d8a", // v3.0.2
   "dessant/lock-threads": "c1b35aecc5cdb1a34539d14196df55838bb2f836", // v4.0.0
-  "amannn/action-semantic-pull-request": "v5.0.2",
+  "amannn/action-semantic-pull-request":
+    "c3cd5d1ea3580753008872425915e343e351ab54", // v5.2.0
   "actions/setup-node": "64ed1c7eab4cce3362f8c340dee64e5eaeef8f7c", // v3.6.0
   "actions/stale": "1160a2240286f5da8ec72b1c0816ce2481aabf84", // v8.0.0
   "peter-evans/create-pull-request": "284f54f989303d2699d373481a0cfa13ad5a6666", // v5.0.1
