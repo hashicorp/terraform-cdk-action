c427ff159adc85f1005c60546d69da50398e528f
Nara Kasbergen <nara.kasbergen@gmail.com>
2023-09-06 11:49:46 +0200

    chore: update Projen & implement best practices
    Updates this repo to start using best practices for Projen projects
    that we've been using elsewhere, e.g. enabling automerge, etc. Also
    updates all GitHub Actions to the latest versions approved by TSCCR.

---
+++ projenrc/auto-approve.ts
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { javascript } from "projen";
+import { JobPermission } from "projen/lib/github/workflows-model";
+
+/**
+ * Approves PRs with the "auto-approve" label
+ */
+export class AutoApprove {
+  constructor(project: javascript.NodeProject) {
+    const workflow = project.github?.addWorkflow("auto-approve");
+
+    if (!workflow) throw new Error("no workflow defined");
+
+    workflow.on({
+      pullRequest: {
+        types: ["opened", "labeled", "ready_for_review", "reopened"],
+      },
+    });
+
+    (workflow.concurrency as any) = "${{ github.workflow }}-${{ github.ref }}";
+
+    const commentText =
+      '"Since I authored this PR, I can\'t approve it myself, sorry! Someone else will need to approve it."';
+
+    workflow.addJobs({
+      approve: {
+        runsOn: ["ubuntu-latest"],
+        env: {
+          PR_ID: "${{ github.event.pull_request.number }}",
+        },
+        if: "contains(github.event.pull_request.labels.*.name, 'auto-approve') && github.event.pull_request.draft == false",
+        steps: [
+          {
+            name: "Checkout PR",
+            uses: "actions/checkout@v3",
+            with: {
+              ref: "${{ github.event.pull_request.head.ref }}",
+              repository:
+                "${{ github.event.pull_request.head.repo.full_name }}",
+            },
+          },
+          {
+            name: "Auto-approve PRs by other users as team-tf-cdk",
+            if: "github.event.pull_request.user.login != 'team-tf-cdk'",
+            run: "gh pr review $PR_ID --approve",
+            env: {
+              GH_TOKEN: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+            },
+          },
+          {
+            name: "Post a note explaining we can't auto-approve PRs by team-tf-cdk",
+            if: "github.event.pull_request.user.login == 'team-tf-cdk'",
+            run:
+              "gh pr comment $PR_ID --body " +
+              commentText +
+              '\ngh pr edit $PR_ID --remove-label "auto-approve"',
+            env: {
+              GH_TOKEN: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+            },
+          },
+        ],
+        permissions: {
+          contents: JobPermission.READ,
+        },
+      },
+    });
+  }
+}
---
+++ projenrc/automerge.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { javascript } from "projen";
+import { JobPermission } from "projen/lib/github/workflows-model";
+
+/**
+ * Merges PRs with the "automerge" label
+ */
+export class Automerge {
+  constructor(project: javascript.NodeProject) {
+    const workflow = project.github?.addWorkflow("automerge");
+
+    if (!workflow) throw new Error("no workflow defined");
+
+    workflow.on({
+      pullRequest: {
+        types: [
+          "opened",
+          "labeled",
+          "ready_for_review",
+          "reopened",
+          "synchronize",
+        ],
+      },
+    });
+
+    (workflow.concurrency as any) = "${{ github.workflow }}-${{ github.ref }}";
+
+    workflow.addJobs({
+      automerge: {
+        runsOn: ["ubuntu-latest"],
+        if: "contains(github.event.pull_request.labels.*.name, 'automerge') && github.event.pull_request.draft == false",
+        steps: [
+          {
+            name: "Checkout",
+            uses: "actions/checkout@v3",
+          },
+          {
+            name: "Turn on automerge for this PR",
+            run: "gh pr merge --auto --squash ${{ github.event.pull_request.number }}",
+            env: {
+              GH_TOKEN: "${{ secrets.PROJEN_GITHUB_TOKEN }}",
+            },
+          },
+        ],
+        permissions: {
+          contents: JobPermission.READ,
+        },
+      },
+    });
+  }
+}
