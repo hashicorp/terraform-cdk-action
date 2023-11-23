b06f73d5edc0e6c11c9d03e42c933b6b9c31431a
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-11-23 20:00:14 +0100

    chore(deps): upgrade actions/github-script & remove node-fetch (#110)

--- scripts/check-terraform-version.js
+++ scripts/check-terraform-version.js
@@ -2,8 +2,6 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
-const fetch = require("node-fetch"); // @TODO this can be removed once we upgrade to Node 18 and use native fetch
-
 async function getLatestVersion() {
   const response = await fetch("https://api.releases.hashicorp.com/v1/releases/terraform/latest");
   const data = await response.json();
