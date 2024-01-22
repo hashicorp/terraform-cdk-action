ee18ace19bf9066b8f7a14a6618f85b8944cff7a
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2024-01-22 12:01:30 +0100

    chore!: change default Terraform version to 1.7.0 (#138)
    This PR increases the default version of Terraform used from `1.6.6` to
    version `1.7.0`.
    This is considered a breaking change because anyone who does not
    manually specify a `terraformVersion` in their action configuration will
    automatically start using the new version.
     
    Unfortunately, not everything can be automated, and the following steps
    need to be completed manually:
     
    - [x] Update the _Terraform Version_ to `1.7.0` in the TFC web UI for
    the
    [cdk-action-testing](https://app.terraform.io/app/cdktf/workspaces/cdk-action-testing/settings/general)
    workspace
     
    Please complete the above steps and then mark this PR as ready for
    review to rerun the checks. Thanks!
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>
    Co-authored-by: Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -43,7 +43,7 @@ const inputs = {
   },
   terraformVersion: {
     description: "The version of Terraform to use",
-    default: "1.6.6",
+    default: "1.7.0",
     required: false,
     type: "string",
   },
