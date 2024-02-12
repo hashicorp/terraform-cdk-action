ed0bfe7e479640a8a845286ea388697187c36e85
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2024-02-12 12:36:00 +0100

    fix: change default Terraform version to 1.7.3 (#148)
    This PR increases the default version of Terraform used from `1.7.2` to
    version `1.7.3`.
    This is not considered a breaking change because it's just a patch
    release that shouldn't have any backwards incompatibilities.
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -43,7 +43,7 @@ const inputs = {
   },
   terraformVersion: {
     description: "The version of Terraform to use",
-    default: "1.7.2",
+    default: "1.7.3",
     required: false,
     type: "string",
   },
