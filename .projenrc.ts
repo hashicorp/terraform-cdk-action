57287d581f109d38d9f000819b967b9d1c6a8326
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2024-03-25 09:48:46 +0100

    fix: change default CDKTF version to 0.20.5 (#166)
    This PR increases the default version of CDKTF used from `0.20.4` to
    version `0.20.5`.
    This is not considered a breaking change because it's just a patch
    release that shouldn't have any backwards incompatibilities.
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -37,7 +37,7 @@ const githubActionPinnedVersions = {
 const inputs = {
   cdktfVersion: {
     description: "The version of CDKTF to use",
-    default: "0.20.4",
+    default: "0.20.5",
     required: false,
     type: "string",
   },
