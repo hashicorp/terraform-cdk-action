a6b414e23415cd1fdedb2ff7930c4df943ffdcb7
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2024-04-08 14:06:39 +0200

    chore(ci): workflow housekeeping (#172)

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,21 +3,7 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { TextFile } from "projen";
-import { JobPermission } from "projen/lib/github/workflows-model";
-import { UpgradeDependenciesSchedule } from "projen/lib/javascript";
-import {
-  GitHubActionTypeScriptProject,
-  RunsUsing,
-} from "projen-github-action-typescript";
-import { AutoApprove } from "./projenrc/auto-approve";
-import { Automerge } from "./projenrc/automerge";
-import { CustomizedLicense } from "./projenrc/customized-license";
-import { LockIssues } from "./projenrc/lock-issues";
-import { UpdateGitTags } from "./projenrc/update-tags";
-import { UpgradeCDKTF } from "./projenrc/upgrade-cdktf";
-import { UpgradeNode } from "./projenrc/upgrade-node";
-import { UpgradeTerraform } from "./projenrc/upgrade-terraform";
+import { TerraformCdkActionProject } from "./projenrc";
 
 const githubActionPinnedVersions = {
   "actions/checkout": "b4ffde65f46336ab88eb53be808477a3936bae11", // v4.1.1
@@ -29,7 +15,7 @@ const githubActionPinnedVersions = {
   "amannn/action-semantic-pull-request":
     "e9fabac35e210fea40ca5b14c0da95a099eff26f", // v5.4.0
   "dessant/lock-threads": "1bf7ec25051fe7c00bdd17e6a7cf3d7bfb7dc771", // v5.0.1
-  "peter-evans/create-pull-request": "a4f52f8033a6168103c2538976c07b467e8163bc", // v6.0.1
+  "peter-evans/create-pull-request": "70a41aba780001da0a30141984ae2a0c95d8704e", // v6.0.2
   "pr-mpt/actions-semver-aliases": "01b2241f545f14efe72edaa2fcec49705dbe910d", // v2.0.0
   "slackapi/slack-github-action": "6c661ce58804a1a20f6dc5fbee7f0381b469e001", // v1.25.0
 };
@@ -105,198 +91,26 @@ const inputs = {
 };
 
 const repoName = "terraform-cdk-action";
-const project = new GitHubActionTypeScriptProject({
-  name: repoName,
-  description:
-    "The Terraform CDK GitHub Action allows you to run CDKTF as part of your CI/CD workflow.",
-  repository: `https://github.com/hashicorp/${repoName}.git`,
-  authorName: "HashiCorp",
-  authorUrl: "https://hashicorp.com",
-  authorOrganization: true,
-  minMajorVersion: 1, // should only be set once you are ready for a 1.0 release!
-  defaultReleaseBranch: "main",
-  projenrcTs: true,
-  prettier: true,
-  licensed: false,
-  pullRequestTemplate: false,
-  mergify: false,
-  depsUpgradeOptions: {
-    workflowOptions: {
-      labels: ["automerge", "dependencies"],
-      schedule: UpgradeDependenciesSchedule.WEEKLY,
-    },
-  },
-  workflowGitIdentity: {
-    name: "team-tf-cdk",
-    email: "github-team-tf-cdk@hashicorp.com",
-  },
-  stale: true,
-  staleOptions: {
-    issues: {
-      exemptLabels: ["backlog", "help wanted"],
-      staleLabel: "stale",
-      daysBeforeStale: 30,
-      staleMessage:
-        "Hi there! 👋 We haven't heard from you in 30 days and would like to know if the problem has been resolved or if " +
-        "you still need help. If we don't hear from you before then, I'll auto-close this issue in 30 days.",
-      daysBeforeClose: 30,
-      closeMessage:
-        "I'm closing this issue because we haven't heard back in 60 days. ⌛️ If you still need help, feel free to reopen the issue!",
-    },
-    pullRequest: {
-      exemptLabels: ["backlog", "help wanted"],
-      staleLabel: "stale",
-      daysBeforeStale: 60,
-      staleMessage:
-        "Hi there! 👋 We haven't heard from you in 60 days and would like to know if you're still working on this or need help. " +
-        "If we don't hear from you before then, I'll auto-close this PR in 30 days.",
-      daysBeforeClose: 30,
-      closeMessage:
-        "I'm closing this pull request because we haven't heard back in 90 days. ⌛️ If you're still working on this, feel free to reopen the PR or create a new one!",
-    },
-  },
+const description =
+  "The Terraform CDK GitHub Action allows you to run CDKTF as part of your CI/CD workflow.";
 
-  actionMetadata: {
+const project = new TerraformCdkActionProject({
+  name: repoName,
+  description,
+  inputs,
+  metadata: {
     author: "HashiCorp, Inc.",
-    description:
-      "The Terraform CDK GitHub Action allows you to run CDKTF as part of your CI/CD workflow.",
+    description,
     branding: {
       color: "purple",
       icon: "terminal",
     },
-    inputs: Object.entries(inputs).reduce(
-      (acc, [key, value]) => ({ ...acc, [key]: { ...value, type: undefined } }),
-      {}
-    ),
-    runs: {
-      using: RunsUsing.NODE_20,
-      main: "dist/index.js",
-    },
   },
-
-  deps: [
-    "@actions/exec",
-    "@actions/io",
-    "@actions/tool-cache",
-    "@hashicorp/js-releases@^1.7.0",
-  ],
-  devDeps: [
-    "projen-github-action-typescript@^0.0.395",
-    "@types/fs-extra",
-    "action-docs",
-  ],
-  peerDeps: ["constructs@^10.0.0"],
-  minNodeVersion: "20.9.0",
 });
-
-new Automerge(project);
-new AutoApprove(project);
-new CustomizedLicense(project);
-new LockIssues(project);
-new UpdateGitTags(project);
-new UpgradeCDKTF(project);
-new UpgradeNode(project);
-new UpgradeTerraform(project);
-
-new TextFile(project, "src/inputs.ts", {
-  committed: true,
-  marker: true,
-  lines: [
-    `/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
-import * as core from "@actions/core";`,
-    "",
-    Object.entries(inputs)
-      .map(
-        ([key, value]) =>
-          `export const ${key}: ${value.type} = core.${
-            value.type === "boolean" ? "getBooleanInput" : "getInput"
-          }("${key}");`
-      )
-      .join("\n"),
-    "",
-  ],
-});
-project.prettier?.addIgnorePattern("src/inputs.ts");
-
-project.projectBuild.postCompileTask.exec("npx action-docs --no-banner -u");
-
-project.addPackageIgnore("scripts");
-project.addPackageIgnore("examples");
-project.addPackageIgnore("projenrc");
-project.addPackageIgnore("/.projenrc.ts");
-
-project.addPackageIgnore(".copywrite.hcl");
-// Add copywrite headers to all files
-project.buildWorkflow?.addPostBuildSteps(
-  {
-    name: "Setup Copywrite tool",
-    uses: "hashicorp/setup-copywrite@867a1a2a064a0626db322392806428f7dc59cb3e", // v1.1.2
-  },
-  { name: "Add headers using Copywrite tool", run: "copywrite headers" }
-);
 
 // Use pinned versions of github actions
 Object.entries(githubActionPinnedVersions).forEach(([name, sha]) => {
   project.github?.actions.set(name, `${name}@${sha}`);
 });
-
-// Add a step to notify Slack after a successful release
-// This is because we can't automate updating the Marketplace, sadly
-project.release?.addJobs({
-  release_notification: {
-    name: "Notify Slack about the release",
-    needs: ["release_github"],
-    runsOn: ["ubuntu-latest"],
-    permissions: {
-      contents: JobPermission.READ,
-    },
-    steps: [
-      {
-        name: "git checkout",
-        uses: "actions/checkout@v3",
-        with: {
-          "fetch-depth": 0,
-        },
-      },
-      {
-        name: "Get the latest tag (version) from git",
-        id: "git_label",
-        run: 'echo "version=$(git describe --tags)" >> $GITHUB_OUTPUT',
-      },
-      {
-        name: "Notify Slack via a custom Workflow webhook",
-        uses: "slackapi/slack-github-action@v1",
-        env: { SLACK_WEBHOOK_URL: "${{ secrets.SLACK_WEBHOOK_URL }}" },
-        with: {
-          payload: JSON.stringify({
-            repository: repoName,
-            version: "${{ steps.git_label.outputs.version }}",
-          }),
-        },
-      },
-    ],
-  },
-});
-
-const releaseWorkflow = project.tryFindObjectFile(
-  ".github/workflows/release.yml"
-);
-releaseWorkflow?.addOverride("on.push", {
-  branches: ["main"],
-  paths: [
-    // only publish a release if these files were changed in any way
-    "dist/**",
-    "action.yml",
-  ],
-});
-// The below is necessary in order to allow the git-tags workflow to run
-releaseWorkflow?.addOverride(
-  "jobs.release_github.steps.3.env.GITHUB_TOKEN",
-  "${{ secrets.PROJEN_GITHUB_TOKEN }}"
-);
 
 project.synth();
---
+++ projenrc/index.ts
@@ -0,0 +1,253 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import { TextFile } from "projen";
+import { JobPermission } from "projen/lib/github/workflows-model";
+import { UpgradeDependenciesSchedule } from "projen/lib/javascript";
+import {
+  GitHubActionMetadata,
+  GitHubActionTypeScriptOptions,
+  GitHubActionTypeScriptProject,
+  RunsUsing,
+} from "projen-github-action-typescript";
+import { AutoApprove } from "./auto-approve";
+import { Automerge } from "./automerge";
+import { CustomizedLicense } from "./customized-license";
+import { LockIssues } from "./lock-issues";
+import { UpdateGitTags } from "./update-tags";
+import { UpgradeCDKTF } from "./upgrade-cdktf";
+import { UpgradeNode } from "./upgrade-node";
+import { UpgradeTerraform } from "./upgrade-terraform";
+import { generateRandomCron, Schedule } from "./util/random-cron";
+
+type GithubActionInput = {
+  type: string;
+  /** A `string` description of the input parameter. */
+  description: string;
+  /** A `boolean` to indicate whether the action requires the input parameter. Set to true when the parameter is required. */
+  required: boolean;
+  /** A `string` representing the default value. The default value is used when an input parameter isn't specified in a workflow file. */
+  default?: string;
+  /** If the input parameter is used, this `string` is logged as a warning message. You can use this warning to notify users that the input is deprecated and mention any alternatives. */
+  deprecationMessage?: string;
+};
+
+export interface TerraformCdkActionOptions
+  extends Partial<GitHubActionTypeScriptOptions> {
+  readonly metadata: Partial<GitHubActionMetadata>;
+  readonly inputs: {
+    [inputName: string]: GithubActionInput;
+  };
+}
+
+export class TerraformCdkActionProject extends GitHubActionTypeScriptProject {
+  constructor(options: TerraformCdkActionOptions) {
+    const { name, description, inputs, metadata } = options;
+
+    super({
+      name: name ?? "terraform-cdk-action",
+      description,
+      repository: `https://github.com/hashicorp/${name}.git`,
+      authorName: "HashiCorp",
+      authorUrl: "https://hashicorp.com",
+      authorOrganization: true,
+      minMajorVersion: 1, // should only be set once you are ready for a 1.0 release!
+      defaultReleaseBranch: "main",
+      projenrcTs: true,
+      prettier: true,
+      licensed: false,
+      pullRequestTemplate: false,
+      mergify: false,
+      depsUpgradeOptions: {
+        workflowOptions: {
+          labels: ["automerge", "dependencies"],
+          schedule: UpgradeDependenciesSchedule.WEEKLY,
+        },
+      },
+      workflowGitIdentity: {
+        name: "team-tf-cdk",
+        email: "github-team-tf-cdk@hashicorp.com",
+      },
+      stale: false, // disabling for now but keeping the options below so we can turn it back on if desired
+      staleOptions: {
+        issues: {
+          exemptLabels: ["backlog", "help wanted"],
+          staleLabel: "stale",
+          daysBeforeStale: 30,
+          staleMessage:
+            "Hi there! 👋 We haven't heard from you in 30 days and would like to know if the problem has been resolved or if " +
+            "you still need help. If we don't hear from you before then, I'll auto-close this issue in 30 days.",
+          daysBeforeClose: 30,
+          closeMessage:
+            "I'm closing this issue because we haven't heard back in 60 days. ⌛️ If you still need help, feel free to reopen the issue!",
+        },
+        pullRequest: {
+          exemptLabels: ["backlog", "help wanted"],
+          staleLabel: "stale",
+          daysBeforeStale: 60,
+          staleMessage:
+            "Hi there! 👋 We haven't heard from you in 60 days and would like to know if you're still working on this or need help. " +
+            "If we don't hear from you before then, I'll auto-close this PR in 30 days.",
+          daysBeforeClose: 30,
+          closeMessage:
+            "I'm closing this pull request because we haven't heard back in 90 days. ⌛️ If you're still working on this, feel free to reopen the PR or create a new one!",
+        },
+      },
+      actionMetadata: {
+        ...metadata,
+        inputs: Object.entries(inputs).reduce(
+          (acc, [key, value]) => ({
+            ...acc,
+            [key]: { ...value, type: undefined },
+          }),
+          {}
+        ),
+        runs: {
+          using: RunsUsing.NODE_20,
+          main: "dist/index.js",
+        },
+      },
+      deps: [
+        "@actions/exec",
+        "@actions/io",
+        "@actions/tool-cache",
+        "@hashicorp/js-releases@^1.7.0",
+      ],
+      devDeps: [
+        "projen-github-action-typescript@^0.0.395",
+        "@types/fs-extra",
+        "action-docs",
+      ],
+      peerDeps: ["constructs@^10.0.0"],
+      minNodeVersion: "20.9.0",
+    });
+
+    new Automerge(this);
+    new AutoApprove(this);
+    new CustomizedLicense(this);
+    new LockIssues(this);
+    new UpdateGitTags(this);
+    new UpgradeCDKTF(this);
+    new UpgradeNode(this);
+    new UpgradeTerraform(this);
+
+    new TextFile(this, "src/inputs.ts", {
+      committed: true,
+      marker: true,
+      lines: [
+        `/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import * as core from "@actions/core";`,
+        "",
+        Object.entries(inputs)
+          .map(
+            ([key, value]) =>
+              `export const ${key}: ${value.type} = core.${
+                value.type === "boolean" ? "getBooleanInput" : "getInput"
+              }("${key}");`
+          )
+          .join("\n"),
+        "",
+      ],
+    });
+    this.prettier?.addIgnorePattern("src/inputs.ts");
+    this.eslint?.addIgnorePattern("src/inputs.ts");
+
+    this.projectBuild.postCompileTask.exec("npx action-docs --no-banner -u");
+
+    this.addPackageIgnore("scripts");
+    this.addPackageIgnore("examples");
+    this.addPackageIgnore("projenrc");
+    this.addPackageIgnore("/.projenrc.ts");
+
+    this.addPackageIgnore(".copywrite.hcl");
+    // Add copywrite headers to all files
+    this.buildWorkflow?.addPostBuildSteps(
+      {
+        name: "Setup Copywrite tool",
+        uses: "hashicorp/setup-copywrite@867a1a2a064a0626db322392806428f7dc59cb3e", // v1.1.2
+      },
+      { name: "Add headers using Copywrite tool", run: "copywrite headers" }
+    );
+
+    // Add a step to notify Slack after a successful release
+    // This is because we can't automate updating the Marketplace, sadly
+    this.release?.addJobs({
+      release_notification: {
+        name: "Notify Slack about the release",
+        needs: ["release_github"],
+        runsOn: ["ubuntu-latest"],
+        permissions: {
+          contents: JobPermission.READ,
+        },
+        steps: [
+          {
+            name: "git checkout",
+            uses: "actions/checkout@v3",
+            with: {
+              "fetch-depth": 0,
+            },
+          },
+          {
+            name: "Get the latest tag (version) from git",
+            id: "git_label",
+            run: 'echo "version=$(git describe --tags)" >> $GITHUB_OUTPUT',
+          },
+          {
+            name: "Notify Slack via a custom Workflow webhook",
+            uses: "slackapi/slack-github-action@v1",
+            env: { SLACK_WEBHOOK_URL: "${{ secrets.SLACK_WEBHOOK_URL }}" },
+            with: {
+              payload: JSON.stringify({
+                repository: name,
+                version: "${{ steps.git_label.outputs.version }}",
+              }),
+            },
+          },
+        ],
+      },
+    });
+
+    const staleWorkflow = this.tryFindObjectFile(".github/workflows/stale.yml");
+    staleWorkflow?.addOverride("on.schedule", [
+      {
+        cron: generateRandomCron({ project: this, maxHour: 4, hourOffset: 1 }),
+      },
+    ]);
+
+    const upgradeWorkflow = this.tryFindObjectFile(
+      ".github/workflows/upgrade-main.yml"
+    );
+    upgradeWorkflow?.addOverride("on.schedule", [
+      {
+        cron: generateRandomCron({
+          project: this,
+          maxHour: 2,
+          schedule: Schedule.Weekly,
+        }),
+      },
+    ]);
+
+    const releaseWorkflow = this.tryFindObjectFile(
+      ".github/workflows/release.yml"
+    );
+    releaseWorkflow?.addOverride("on.push", {
+      branches: ["main"],
+      paths: [
+        // only publish a release if these files were changed in any way
+        "dist/**",
+        "action.yml",
+      ],
+    });
+    // The below is necessary in order to allow the git-tags workflow to run
+    releaseWorkflow?.addOverride(
+      "jobs.release_github.steps.3.env.GITHUB_TOKEN",
+      "${{ secrets.PROJEN_GITHUB_TOKEN }}"
+    );
+  }
+}
