8074795603219e9e493b9a5c339e08ca78eed6b2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-07-01 12:21:13 +0000

    fix: change default CDKTF version to 0.20.8
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -23,7 +23,7 @@ const githubActionPinnedVersions = {
 const inputs = {
   cdktfVersion: {
     description: "The version of CDKTF to use",
-    default: "0.20.7",
+    default: "0.20.8",
     required: false,
     type: "string",
   },
