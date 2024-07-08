16eb0afd4a2f262ffab5a4739b641422536aefc1
Nara Kasbergen <nara.kasbergen@gmail.com>
2024-07-08 16:11:24 +0200

    chore(deps): update pinned versions of GitHub Actions

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,16 +6,17 @@
 import { TerraformCdkActionProject } from "./projenrc";
 
 const githubActionPinnedVersions = {
-  "actions/checkout": "a5ac7e51b41094c92402da3b24376905380afc29", // v4.1.6
+  "actions/checkout": "692973e3d937129bcbf40652eb9f2f61becf3332", // v4.1.7
   "actions/download-artifact": "65a9edc5881444af0b9093a5e628f2fe47ea3b2e", // v4.1.7
   "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
   "actions/setup-node": "60edb5dd545a775178f52524783378180af0d1f8", // v4.0.2
   "actions/stale": "28ca1036281a5e5922ead5184a1bbf96e5fc984e", // v9.0.0
   "actions/upload-artifact": "65462800fd760344b1a7b4382951275a0abb4808", // v4.3.3
   "amannn/action-semantic-pull-request":
-    "cfb60706e18bc85e8aec535e3c577abe8f70378e", // v5.5.2
+    "0723387faaf9b38adef4775cd42cfd5155ed6017", // v5.5.3
   "dessant/lock-threads": "1bf7ec25051fe7c00bdd17e6a7cf3d7bfb7dc771", // v5.0.1
-  "peter-evans/create-pull-request": "6d6857d36972b65feb161a90e484f2984215f83e", // v6.0.5
+  "hashicorp/setup-copywrite": "32638da2d4e81d56a0764aa1547882fc4d209636", // v1.1.3
+  "peter-evans/create-pull-request": "c5a7806660adbe173f04e3e038b0ccdcd758773c", // v6.1.0
   "pr-mpt/actions-semver-aliases": "01b2241f545f14efe72edaa2fcec49705dbe910d", // v2.0.0
   "slackapi/slack-github-action": "70cd7be8e40a46e8b0eced40b0de447bdb42f68e", // v1.26.0
 };
