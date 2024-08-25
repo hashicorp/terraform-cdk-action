ae2f934e011ed2c7b819f6482007f9cfe3dec8ca
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-25 23:41:46 +0000

    fix: change default Terraform version to 1.9.5
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -30,7 +30,7 @@ const inputs = {
   },
   terraformVersion: {
     description: "The version of Terraform to use",
-    default: "1.9.4",
+    default: "1.9.5",
     required: false,
     type: "string",
   },
