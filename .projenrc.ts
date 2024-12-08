54c884bcd3cd5a9c981a8d1cc6c6ec5c1a4c0ac2
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-08 23:42:28 +0000

    chore!: change default Terraform version to 1.10.1
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -30,7 +30,7 @@ const inputs = {
   },
   terraformVersion: {
     description: "The version of Terraform to use",
-    default: "1.9.8",
+    default: "1.10.1",
     required: false,
     type: "string",
   },
