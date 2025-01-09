24cd9ba3eb91a58cd9e0cec5c443d4656dc93c89
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2025-01-09 15:42:05 +0100

    chore(ci): update required version of constructs when cdktf is upgraded (#245)

--- scripts/update-cdktf.sh
+++ scripts/update-cdktf.sh
@@ -6,16 +6,22 @@ set -ex
 
 PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
 CDKTF_VERSION=$1
+CONSTRUCTS_VERSION=$2
 
 if [ -z "$CDKTF_VERSION" ]; then
-  echo "Usage: $0 <cdktf-version>"
+  echo "Usage: $0 <cdktf-version> <constructs-version>"
+  exit 1
+fi
+if [ -z "$CONSTRUCTS_VERSION" ]; then
+  echo "Usage: $0 <cdktf-version> <constructs-version>"
   exit 1
 fi
 
-echo "Updating default cdktf version to $CDKTF_VERSION"
+echo "Updating default cdktf version to $CDKTF_VERSION and constructs version $CONSTRUCTS_VERSION"
 yarn
 # HACK ALERT! This will stop working once CDKTF goes 1.0+
 sed -i 's/default: "0\..*",/default: "'"$CDKTF_VERSION"'",/' "$PROJECT_ROOT/.projenrc.ts"
+sed -i "s/constructsVersion = \".*\";/constructsVersion = \"$CONSTRUCTS_VERSION\";/" "$PROJECT_ROOT/projenrc/index.ts"
 CI=0 npx projen
 npx projen build
 
