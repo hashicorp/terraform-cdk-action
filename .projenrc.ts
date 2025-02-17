bffbdef03e92de62007b9c7891b55dab5d421a6f
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2025-02-17 12:37:40 +0100

    chore(deps): update pinned versions of GitHub Actions (#263)

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,9 +9,9 @@ const githubActionPinnedVersions = {
   "actions/checkout": "11bd71901bbe5b1630ceea73d27597364c9af683", // v4.2.2
   "actions/download-artifact": "fa0a91b85d4f404e444e00e005971372dc801d16", // v4.1.8
   "actions/github-script": "60a0d83039c74a4aee543508d2ffcb1c3799cdea", // v7.0.1
-  "actions/setup-node": "39370e3970a6d050c480ffad4ff0ed4d3fdee5af", // v4.1.0
-  "actions/stale": "28ca1036281a5e5922ead5184a1bbf96e5fc984e", // v9.0.0
-  "actions/upload-artifact": "6f51ac03b9356f520e9adb1b1b7802705f340c2b", // v4.5.0
+  "actions/setup-node": "1d0ff469b7ec7b3cb9d8673fde0c81c44821de2a", // v4.2.0
+  "actions/stale": "5bef64f19d7facfb25b37b414482c7164d639639", // v9.1.0
+  "actions/upload-artifact": "65c4c4a1ddee5b72f698fdd19549f0f0fb45cf08", // v4.6.0
   "amannn/action-semantic-pull-request":
     "0723387faaf9b38adef4775cd42cfd5155ed6017", // v5.5.3
   "dessant/lock-threads": "1bf7ec25051fe7c00bdd17e6a7cf3d7bfb7dc771", // v5.0.1
