64770d17c1feb533ea2cfac1df54ffec09e3a0d0
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2025-06-04 20:35:23 +0200

    chore!: change default CDKTF version to 0.21.0 (#290)
    This PR increases the default version of CDKTF used from `0.20.12` to
    version `0.21.0`.
    This is considered a breaking change because anyone who does not
    manually specify a `cdktfVersion` in their action configuration will
    automatically start using the new version.
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -24,7 +24,7 @@ const githubActionPinnedVersions = {
 const inputs = {
   cdktfVersion: {
     description: "The version of CDKTF to use",
-    default: "0.20.12",
+    default: "0.21.0",
     required: false,
     type: "string",
   },
